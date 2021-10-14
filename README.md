bffa7af3cf560f1e111ff5c6c3da4607f19e468e
grdAll <alex.thogar@gmail.com>
2021-10-14 20:12:42 +0200

    Package defaults

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # python-packages
  Collection of general Python functions.
+
+## Install locally
+
+1. `$ cd [path to this folder]`
+1. `$ python pip install .`
+1. Check installed packages
+  - `$ python pip list`
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name="util",
+version="0.1",
+description="My utils",
+url="#",
+author="",
+install_requires=["requests>=2.0.0"],
+author_email="",
+packages=setuptools.find_packages(),
+zip_safe=False)
