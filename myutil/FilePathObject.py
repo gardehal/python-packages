e01869a4a5455b7990b8529dc86580ee44a62a79
grdAll <alex.thogar@gmail.com>
2021-11-24 00:30:35 +0100

    Update FilePathObject.py

--- myutil/FilePathObject.py
+++ myutil/FilePathObject.py
@@ -2,36 +2,70 @@ import os
 
 class FilePathObject():
     def __init__(self, fullPath = "", directory = "", relativePath = "", fileLeaf = "", filename = "", fileRoot = "", extension = "", extensionWithDot = ""):
-        self.fullPath = fullPath
-        self.directory = directory
-        self.relativePath = relativePath
-        self.fileLeaf = fileLeaf
-        self.filename = filename
-        self.fileRoot = fileRoot
-        self.extension = extension
-        self.extensionWithDot = extensionWithDot
+        self.fullPath: str = fullPath
+        self.directory: str = directory
+        self.relativePath: str = relativePath
+        self.fileLeaf: str = fileLeaf
+        self.filename: str = filename
+        self.fileRoot: str = fileRoot
+        self.extension: str = extension
+        self.extensionWithDot: str = extensionWithDot
 
-    def __init__(self, path):
+    def __init__(self, path: str, debug: bool = False):
         if(path == None):
+            if(debug): print("FilePathObject: Argument path is None.")
             return None
+
+        if(not (os.path.isdir(path) or os.path.isfile(path))):
+            if(debug): print("FilePathObject: Argument path is not a dir or file.")
+            return None
+
+        _path = path.replace(r"\\\\|\/\/|\/", os.path.sep)
 
         _fullPath = None
         _relativePath = None
-        if(os.path.isabs(path)):
-            _fullPath = path
+        if(os.path.isabs(_path)):
+            if(debug): print("FilePathObject: Argument path is absolute.")
+            _fullPath = _path
             _relativePath = os.path.realpath(_fullPath)
         else:
-            _fullPath = os.path.abspath(path)
-            _relativePath = path
+            if(debug): print("FilePathObject: Argument path is relative.")
+            _fullPath = os.path.abspath(_path)
+            _relativePath = _path
         
-        self.fullPath = _fullPath
-        self.directory = os.path.split(_fullPath)[0]
-        self.relativePath = _relativePath
-        self.fileLeaf = os.path.split(self.directory)[-1]
-        self.filename = os.path.basename(_fullPath)
-        self.fileRoot = ".".join(self.filename.split(".")[:-1])
-        self.extension =  _fullPath.split(".")[-1] if ("." in _fullPath) else ""
-        self.extensionWithDot = f".{self.extension}"
+        self.fullPath: str = _fullPath
+        self.directory: str = os.path.split(_fullPath)[0]
+        self.relativePath: str = _relativePath
+        self.fileLeaf: str = os.path.split(self.directory)[-1]
+        self.filename: str = os.path.basename(_fullPath)
+        self.fileRoot: str = ".".join(self.filename.split(".")[:-1])
+        self.extension: str =  _fullPath.split(".")[-1] if ("." in _fullPath) else ""
+        self.extensionWithDot: str = f".{self.extension}"
+        self.inputPath: str = path
+        self.inputPathClean: str = _path
+
+        if(debug): print("FilePathObject: No errors.")
+
+    def help():
+        """
+        This function is a developer help-workaround and returns None.\n\n
+
+        myObject = FilePathObject(filePath) returns an object with various strings for values in a file URI (more info in file).\n
+        Given URI: C:\\users\\OddThinking\\Documents\\My Source\\Widget\\foo.src
+        - fullPath: str = "C:\\users\\OddThinking\\Documents\\My Source\\Widget\\"\n
+        - directory: str = "C:\\users\\OddThinking\\Documents\\My Source\\Widget\\foo.src"\n
+        - relativePath: str = relative path from current directory\n
+        - fileLeaf: str = "Widget\\foo.src"\n
+        - filename: str = "foo.src"\n
+        - fileRoot: str = "foo"\n
+        - extension: str = ".src"\n
+        - extensionWithDot: str = "src"\n
+        - inputPath: str = input of "path" argument\n
+        - inputPathClean: str = input of "path" argument where separators like "/", "//", "\\\\" bas been replaces with system separator\n
+        """
+
+        return None
+
 
     # https://stackoverflow.com/questions/2235173/what-is-the-naming-standard-for-path-components
     # Consider the URI:
