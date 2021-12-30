8379d4a13cdc7d69e5788c78089070450f05a788
grdAll <alex.thogar@gmail.com>
2021-12-30 23:15:32 +0100

    Update Util.py

--- myutil/Util.py
+++ myutil/Util.py
@@ -196,6 +196,30 @@ def mkdir(dirPath):
     """
     os.makedirs(dirPath, exist_ok=True)
 
+def makeFiles(*args) -> bool:
+    """
+    Create local files used for storing settings, video ques, sources etc.
+
+    Args:
+        args (list): paths+filenames to create
+
+    Returns:
+        bool: success = true
+    """
+
+    for filepath in args:
+        try:
+            if(not os.path.exists(os.path.dirname(filepath))):
+                os.makedirs(os.path.dirname(filepath))
+        except OSError as exc: # Guard against race condition
+            printS("There was a temporary error creating file ", filepath, ".", color=colors["ERROR"])
+            continue
+
+        file = open(filepath, "a")
+        file.close()
+    
+    return True
+
 def printSpinner():
     """
     Displays a small spinner.
