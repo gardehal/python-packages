f288f5e494ebf3a0c2cd5bfad10389a06fbe85d4
Alexander <alex.thogar@gmail.com>
2022-01-08 10:59:09 +0100

    Update Util.py

--- myutil/Util.py
+++ myutil/Util.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import os
 import requests
@@ -240,6 +241,38 @@ def getIfExists(array, index):
     Get the element at index from array, if the length of the array is greater or equal to the index + 1.
     """
     return array[index] if len(array) >= index + 1 else None
+
+def sanitize(*args, mode: int = 1) -> str:
+    """
+    Sanitize a series of values and return them as a single string.
+    Modes:
+    0 - replace everything except latin alphanumerical symbols and space
+    1 - replace everything except latin alphanumerical symbols, whitespace (\\s) 
+    2 - replace everything except latin alphanumerical symbols, whitespace (\\s), and common symbols like < > , . _ - : ; * ^ ! " ' # % & /   (  ) [ ] = + ?
+    TODO - replace commonly known escape characters
+    TODO - replace only worst of escape charaters
+
+    Args:
+        args (*args): any values to sanitize and return as string
+        mode (int, optional): Mode to use. Defaults to 0.
+
+    Returns:
+        str: concatinated, sanitized result
+    """
+    
+    _result = ""
+    for element in args:
+        _element = str(element)
+        
+        if(mode == 0):
+            _result += re.sub("[^a-zA-Z0-9]", "", _element)
+        if(mode == 1):
+            _result += re.sub("[^a-zA-Z0-9\s]", "", _element)
+        if(mode == 2):
+            _result += re.sub("[^a-zA-Z0-9\s\<\>\,\.\_\-\:\;\*\^\!\"\'\#\%\&\/\\\(\)\[\]\=\+\?]", "", _element)
+            
+            
+    return _result
     
 # https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal?rq=1
 colors = {
