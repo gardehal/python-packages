944c1bf72728f3bd0f981aec0f4d18706ca322a3
Alexander <alex.thogar@gmail.com>
2022-01-30 08:50:22 +0100

    Moved enums to enum classes in files

---
+++ myutil/HttpVerb.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class HttpVerb(Enum):
+    GET = 1
+    POST = 2
+    PUT = 3
+    PATCH = 4
+    DELETE = 5
---
+++ myutil/ShellType.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class ShellType(Enum):
+    CMD = 1
+    POWERSHELL = 2
+    BASH = 3
