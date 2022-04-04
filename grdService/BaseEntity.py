e4a1d3e77d979f378d9ac70db102c774372c241b
Alexander <alex.thogar@gmail.com>
2022-04-04 07:40:04 +0200

    Made entity more general

--- grdService/BaseEntity.py
+++ grdService/BaseEntity.py
@@ -4,20 +4,17 @@ from datetime import datetime
 
 class BaseEntity():
     def __init__(self,
-                 dataString: str = None,
                  created: datetime = datetime.now(),
                  updated: datetime = datetime.now(),
                  deleted: datetime = None,
                  id: str = str(uuid.uuid4())):
-        self.dataString: str = dataString
         self.created: datetime = created
         self.updated: datetime = updated
         self.deleted: datetime = deleted
         self.id: str = id
 
     def summaryString(self):
-        return "".join(map(str, ["dataString: ", self.dataString, 
-            ", id: ", self.id]))
+        return "".join(map(str, ["ID: ", self.id]))
 
     def detailsString(self, includeId: bool = True, includeDatetime: bool = True, includeList: bool = True):
         idString = ", id: " + self.id if(includeId) else ""
@@ -25,8 +22,7 @@ class BaseEntity():
         updatedString = ", updated: " + self.updated if(includeDatetime) else ""
         deletedString = ", deleted: " + self.deleted if(includeDatetime) else ""
         
-        return "".join(map(str, ["dataString: ", self.dataString,
-            createdString,
+        return "".join(map(str, [createdString,
             updatedString,
             deletedString,
             idString]))
