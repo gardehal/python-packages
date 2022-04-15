407a294b7234dee4ba0afb3467719d773524ac23
Alexander <alex.thogar@gmail.com>
2022-04-15 11:52:14 +0200

    Added some quit alternatives

--- grdUtil/StaticUtil.py
+++ grdUtil/StaticUtil.py
@@ -2,3 +2,4 @@
 class StaticUtil:
     affirmative = ["affirmative", "start", "go", "yep", "yes", "y", "1"]
     negative = ["negative", "stop", "quit", "end", "no", "n", "0"]
+    quit = ["quit", "q", "exit", "end", "kill", "die", "terminate", "bye"]
