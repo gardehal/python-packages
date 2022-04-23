6d37ff7f37425f08d6f06f1c75ce34503496d2c5
Alexander <alex.thogar@gmail.com>
2022-04-23 21:21:39 +0200

    Added LogLevel for testing

---
+++ grdUtil/LogLevel.py
@@ -0,0 +1,10 @@
+from enum import IntEnum
+
+
+class LogLevel(IntEnum):
+    NONE = 1
+    INFO = 2
+    WARNING = 3
+    ERROR = 4
+    VERBOSE = 5
+    DEBUG = 6
---
+++ grdUtil/LogUtil.py
@@ -0,0 +1,16 @@
+
+from grdUtil.ShellUtil import *
+from LogLevel import LogLevel
+
+
+def logToJson(itemToLog: str, logLevel: LogLevel, doPrint: bool = False) -> bool:
+    """
+    Log a string to a file as plain text.
+    """
+    return True
+
+def logToFile(itemToLog: any, logLevel: LogLevel, doPrint: bool = False) -> bool:
+    """
+    Log a string to a file as plain text.
+    """
+    return True
