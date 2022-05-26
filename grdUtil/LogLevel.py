d4467601386686fa604f2c89b0e12a541e071872
Alexander <alex.thogar@gmail.com>
2022-05-27 00:21:29 +0200

    Corrected logging order

--- grdUtil/LogLevel.py
+++ grdUtil/LogLevel.py
@@ -4,7 +4,7 @@ from enum import IntEnum
 class LogLevel(IntEnum):
     NONE = 1
     INFO = 2
-    WARNING = 3
-    ERROR = 4
-    VERBOSE = 5
+    VERBOSE = 3
+    WARNING = 4
+    ERROR = 5
     DEBUG = 6
