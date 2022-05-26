871122b8783c260e46868a647f02a35820e6adfe
Alexander <alex.thogar@gmail.com>
2022-05-27 00:12:21 +0200

    Logging working for text, will need to rework JsonUtil...

--- grdUtil/LogUtil.py
+++ grdUtil/LogUtil.py
@@ -1,9 +1,12 @@
 
-from datetime import datetime
-from .FileUtil import mkdir
+import inspect
+from datetime import date, datetime, timezone
+import os
+import time
 
-from .LogLevel import LogLevel
+from .FileUtil import mkdir
 from .JsonUtil import toDict
+from .LogLevel import LogLevel
 
 
 class LogUtil():
@@ -13,7 +16,7 @@ class LogUtil():
     
     def __init__(self, logDir: str, debug: bool = True, logLevel: LogLevel = LogLevel.ERROR) -> None:
         """
-        Init class for logging data to files named after date logged.
+        Initiate class for logging data to files named after date logged.
 
         Args:
             logDir (str): Path to directory for logs.
@@ -26,19 +29,32 @@ class LogUtil():
         
         mkdir(logDir)
         
-    def writeLog(self, text: str) -> bool:
+    def writeLog(self, text: str, fileExtension: str = ".md", logTitle: str = "LOGLEVEL\tDATETIME\tCALLING_FUNCTION\tMESSAGE") -> bool:
         """
         Write text to file in logDir.
 
         Args:
             text (str): Text to log.
+            fileExtension (str): Extension of file. Defaults to .md.
+            logTitle (str): Title that appears as the first line. Defaults to LOGLEVEL\tDATETIME\tCALLING_FUNCTION\tMESSAGE.
 
         Returns:
             bool: Result of write.
         """
         
+        dateNow = date.fromtimestamp(time.time()).isoformat()
+        logFileName = "".join([dateNow, fileExtension])
+        logPath = os.path.join(self.logDir, logFileName)
+        
+        if(not os.path.isfile(logPath)):
+            file = open(logPath, "a")
+            file.write(logTitle)
+            file.close()
+            
         try:
-            # TODO
+            file = open(logPath, "a")
+            file.write("".join(["\n", text]))
+            file.close()
             return True
         except:
             return False
@@ -54,7 +70,7 @@ class LogUtil():
             bool: Items should be logged and printed.
         """
         
-        return logLevel > self.logLevel
+        return logLevel.value >= self.logLevel.value
         
     def validateLogLevel(self, logLevel: LogLevel) -> LogLevel:
         """
@@ -67,7 +83,7 @@ class LogUtil():
             LogLevel: Valid LogLevel.
         """
         
-        return logLevel > self.logLevel
+        return logLevel if(logLevel != None) else self.logLevel
         
     def logAsJson(self, entity: any, logLevel: LogLevel = None, doPrint: bool = False) -> str:
         """
@@ -96,9 +112,11 @@ class LogUtil():
     def logAsText(self, *args: any, logLevel: LogLevel = None, doPrint: bool = False) -> str:
         """
         Log arguments arg to file in path self.logDir.
+        
+        Format: LOGLEVEL    DATETIME    CALLING_FUNCTION    MESSAGE
 
         Args:
-            *args (any): Object to log. These will be concat to a string.
+            *args (any): Data to log. These will be concat to a string.
             logLevel (LogLevel, optional): LogLevel to log as. Defaults to None (will use self.logLevel).
             doPrint (bool, optional): Should log result be printed to print(). Defaults to False.
 
@@ -110,11 +128,15 @@ class LogUtil():
         if(not self.shouldLog(validLogLevel)):
             return None
         
-        level = str(validLogLevel.value)
-        now = str(datetime.now)
-        logMessage = [str(_) for _ in args]
-        # LOGLEVEL  DATETIME    MESSAGE
-        log = "".join(level, "\t", now, "\t", logMessage)
+        level = str(validLogLevel.name)
+        now = datetime.utcnow().replace(tzinfo = timezone.utc).isoformat()
+        logMessage = "".join([str(_) for _ in args])
+        
+        currentFrame = inspect.currentframe()
+        callerFrame = inspect.getouterframes(currentFrame, 2)
+        parentMethodName = callerFrame[1][3]
+        
+        log = "".join([level, "\t", now, "\t", parentMethodName, "\t", logMessage])
         logResult = self.writeLog(log)
         if(not logResult):
             return None
