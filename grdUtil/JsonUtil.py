e7a9fce20d5c9d06646541fded32d01eaa76c66e
Alexander <alex.thogar@gmail.com>
2022-05-27 00:28:32 +0200

    Removed JsonUtil class, no just methods

--- grdUtil/JsonUtil.py
+++ grdUtil/JsonUtil.py
@@ -4,93 +4,92 @@ from typing import TypeVar, _GenericAlias
 
 T = TypeVar("T")
 
-class JsonUtil:
-    def toDict(obj: object) -> dict:
-        """
-        Converts objects to dictionaries.
-        Source: https://www.codegrepper.com/code-examples/whatever/python+nested+object+to+dict
+def toDict(obj: object) -> dict:
+    """
+    Converts objects to dictionaries.
+    Source: https://www.codegrepper.com/code-examples/whatever/python+nested+object+to+dict
 
-        Args:
-            obj (object): object to convert
+    Args:
+        obj (object): object to convert
 
-        Returns:
-            dict: dictionary of input object
-        """
+    Returns:
+        dict: dictionary of input object
+    """
+    
+    if not  hasattr(obj,"__dict__"):
+        return obj
+    
+    result = {}
+    for key, val in obj.__dict__.items():
+        if(key.startswith("_")):
+            continue
         
-        if not  hasattr(obj,"__dict__"):
-            return obj
-        
-        result = {}
-        for key, val in obj.__dict__.items():
-            if(key.startswith("_")):
-                continue
-            
+        element = []
+        if(isinstance(val, _GenericAlias)): # E.g. List[str]
             element = []
-            if(isinstance(val, _GenericAlias)): # E.g. List[str]
-                element = []
-            elif(isinstance(val, list)):
-                for item in val:
-                    element.append(JsonUtil.toDict(item))
-            elif(isinstance(val, enum.Enum)):
-                element = val.value
-            else:
-                element = JsonUtil.toDict(val)
-                
-            result[key] = element
-            
-        return result
-    
-    def toJson(obj: object) -> str:
-        """
-        Converts objects to JSON though dictionaries.
-
-        Args:
-            obj (object): object to convert
-
-        Returns:
-            str: JSON string
-        """
-        
-        dict = JsonUtil.toDict(obj)
-        return json.dumps(dict, default=str)
-    
-    def writeToJsonFile(filepath: str, obj: object) -> bool:
-        """
-        Writes object obj to a JSON format in file from filepath.
-
-        Args:
-            filepath (str): path to file to store JSON in
-            obj (object): object to save
-
-        Returns:
-            bool: true = success
-        """
-        
-        asDict = JsonUtil.toDict(obj)
-        with open(filepath, "w") as file:
-            json.dump(asDict, file, indent=4, default=str)
-            
-        return True
-    
-    def readJsonFile(filepath: str, typeT: T) -> T:
-        """
-        Opens and reads a JSON formatted file, returning object of type T.
-
-        Args:
-            filepath (str): path to file to read JSON from
-            typeT (T): object to convert to
-
-        Returns:
-            T or None: object from JSON file, if file is empty: None
-        """
-        
-        fileContent = open(filepath, "r").read()
-        if(len(fileContent) < 2):
-            return None
+        elif(isinstance(val, list)):
+            for item in val:
+                element.append(toDict(item))
+        elif(isinstance(val, enum.Enum)):
+            element = val.value
         else:
-            return JsonUtil.fromJson(fileContent, typeT)
+            element = toDict(val)
+            
+        result[key] = element
+        
+    return result
+
+def toJson(obj: object) -> str:
+    """
+    Converts objects to JSON though dictionaries.
+
+    Args:
+        obj (object): object to convert
+
+    Returns:
+        str: JSON string
+    """
     
-    def fromJson(jsonStr: str, typeT: T) -> T:
+    dict = toDict(obj)
+    return json.dumps(dict, default=str)
+
+def writeToJsonFile(filepath: str, obj: object) -> bool:
+    """
+    Writes object obj to a JSON format in file from filepath.
+
+    Args:
+        filepath (str): path to file to store JSON in
+        obj (object): object to save
+
+    Returns:
+        bool: true = success
+    """
+    
+    asDict = toDict(obj)
+    with open(filepath, "w") as file:
+        json.dump(asDict, file, indent=4, default=str)
+        
+    return True
+
+def readJsonFile(filepath: str, typeT: T) -> T:
+    """
+    Opens and reads a JSON formatted file, returning object of type T.
+
+    Args:
+        filepath (str): path to file to read JSON from
+        typeT (T): object to convert to
+
+    Returns:
+        T or None: object from JSON file, if file is empty: None
+    """
+    
+    fileContent = open(filepath, "r").read()
+    if(len(fileContent) < 2):
+        return None
+    else:
+        return fromJson(fileContent, typeT)
+
+def fromJson(jsonStr: str, typeT: T) -> T:
         """
         Converts JSON to an object T.
 
