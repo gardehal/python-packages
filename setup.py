be8085e3afd41d3352632df88e7595f7e0692f69
Alexander <alex.thogar@gmail.com>
2022-05-28 00:35:21 +0200

    v.1.4.0: Removed JsonUtil class, improved docs, code, varnames

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setuptools.setup(name="grdUtil",
-version="1.3.6",
+version="1.4.0",
 description="Various Python utility methods, printing, input handling, colouring text in Bash, and more.",
 url="https://github.com/grdall/python-packages",
 author="grdAll",
