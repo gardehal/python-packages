f9f6e30a00c1f276ed3a7ba5d265519f74833644
Alexander <alex.thogar@gmail.com>
2022-07-02 14:46:51 +0200

    v1.5.2: Added joinAsString method

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setuptools.setup(name="grdUtil",
-version="1.5.1",
+version="1.5.2",
 description="Various Python utility methods, printing, input handling, colouring text in Bash, and more.",
 url="https://github.com/grdall/python-packages",
 author="grdAll",
