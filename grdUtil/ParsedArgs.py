02239b80f6377b8910dbe1d934a124389686b82a
Alexander <alethogar@protonmail.com>
2025-11-10 04:44:38 +0100

    Renames

--- grdUtil/ParsedArgs.py
+++ grdUtil/ParsedArgs.py
@@ -2,17 +2,17 @@ import os
 
 class ArgHead():
     """
-    Holder of all flags and args
+    Holder of all commands and args
     """
     
-    flags: list[Flag]
-    flagPrefix: str
+    commands: list[Commands]
+    commandPrefix: str
     namedArgDelim: str
     argDelum: str
     
-    def __init__(self, flags: list[Flag], flagPrefix: str = "-", namedArgDelim: str = ":", argDelum: str = " "):
-        self.flags = flags
-        self.flagPrefix = flagPrefix
+    def __init__(self, commands: list[Commands], commandPrefix: str = "-", namedArgDelim: str = ":", argDelum: str = " "):
+        self.commands = commands
+        self.commandPrefix = commandPrefix
         self.namedArgDelim = namedArgDelim
         self.argDelum = argDelum
     
@@ -25,27 +25,27 @@ class ArgHead():
         
         # TODO reduce foreaches
         inputSplit = input.split(self.argDelum)
-        for flag in self.flags.alias:
-            prefixedAlias = [f"{self.flagPrefix}{e}" for e in flag.alias]
+        for command in self.commands.alias:
+            prefixedAlias = [f"{self.commandPrefix}{e}" for e in command.alias]
             for alias in prefixedAlias:
-                flagIndex = inputSplit.index(alias)
-                if(flagIndex):
-                    potentialArgs = inputSplit[flagIndex+1:]
-                    nextFlagIndex = potentialArgs.index(fr"{self.argDelim}{self.flagPrefix}.*")
-                    args = potentialArgs[:nextFlagIndex]
+                commandIndex = inputSplit.index(alias)
+                if(commandIndex):
+                    potentialArgs = inputSplit[commandIndex+1:]
+                    nextCommandIndex = potentialArgs.index(fr"{self.argDelim}{self.commandPrefix}.*")
+                    args = potentialArgs[:nextCommandIndex]
                     
-                    argResult = ArgResult(flag.name, flag.hitValue, flagIndex, [], [], inputSplit[nextFlagIndex:])
+                    argResult = ArgResult(command.name, command.hitValue, commandIndex, [], [], inputSplit[nextCommandIndex:])
                     namedArgs = {}
-                    for argValue in flag.argValues:
-                        # for argument in arguments, intersect with flag.argValues + self.namedArgDelim
+                    for argValue in command.argValues:
+                        # for argument in arguments, intersect with command.argValues + self.namedArgDelim
                         # if match, add to namedArgs dict as argValue.name: argument.split(self.namedArgDelim)[1], if empty, use None? assume next arg is value?
                         
                         argAlias = [f"{e}{self.namedArgDelim}" for e in argValue.alias]
-                        argumentKeys = [e for e in args if(e.__contains__(self.namedArgDelim))]
-                        for arg in args:
-                            print(arg)
-                            if(arg in argAlias):
-                                namedArgs[arg.split(self.namedArgDelim)[0]: args[args.index(arg) + 1]] # awful
+                        argKeys = [e for e in args if(e.__contains__(self.namedArgDelim))]
+                        for key in argKeys:
+                            print(key)
+                            if(key in argAlias):
+                                namedArgs[key.split(self.namedArgDelim)[0]: args[args.index(key) + 1]] # awful
                         
                         #find positional arguments, prioritized - ignore named args? include named args in position? 
                         # - get and removed named, then check positional
@@ -57,9 +57,9 @@ class ArgHead():
         
         return result
     
-class Flag():
+class Commands():
     """
-    Designates flags
+    Designates commands
     eg. height in 
     $ -height value:100
     """
@@ -77,7 +77,7 @@ class Flag():
         
 class ArgValue():
     """
-    Designates values input as args to flags 
+    Designates values input as args to commands 
     eg. value in 
     $ -height value:100
     """
@@ -98,19 +98,19 @@ class ArgValue():
 
 class ArgsResult():
     """
-    Returns of validate, with info of what flag was hit, what values was added, where it was in the input string, what to parse next for the caller
+    Returns of validate, with info of what command was hit, what values was added, where it was in the input string, what to parse next for the caller
     """
-    flagName: str
+    commandName: str
     hitValue: str # any
-    flagIndex: int
+    commandIndex: int
     argValues: list[ArgValue] # struct? str:any?
     unhandledInputs: list[str]
     nextInput: str
 
-    def __init__(self, flagName: str, hitValue: str, flagIndex: int, argValues: list[ArgValue], unhandledInputs: list[str], nextInput: str):
-        self.flagName = flagName
+    def __init__(self, commandName: str, hitValue: str, commandIndex: int, argValues: list[ArgValue], unhandledInputs: list[str], nextInput: str):
+        self.commandName = commandName
         self.hitValue = hitValue
-        self.flagIndex = flagIndex
+        self.commandIndex = commandIndex
         self.argValues = argValues
         self.unhandledInputs = unhandledInputs
         self.nextInput = nextInput
